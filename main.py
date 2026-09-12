b7ceedf0768fe6b2c18b62be5cab63c4ad847ec7
Matias Daniel Ibañez <sm.matias370@gmail.com>
2026-09-12 13:24:22 -0300

    Permite consultar varios clientes en una sesion

--- main.py
+++ main.py
@@ -2,24 +2,30 @@
 
 print ("===Gestor de cobranzas ===")
 
-cliente = input("Nombre del cliente: ")
+while True:
 
-try:
-    deuda = float(input("Deuda inicial: "))
-    pago = float(input("Pago recibido: "))
-except ValueError:
-    print("Error: ingresá números. Para decimales, usá punto.")
-else:
-     
-    if deuda < 0 or pago <0:
-        print("Error: La deuda y el pago no pueden ser negativos.")
+    cliente = input("Nombre del cliente: ")
+
+    try:
+        deuda = float(input("Deuda inicial: "))
+        pago = float(input("Pago recibido: "))
+    except ValueError:
+        print("Error: ingresá números. Para decimales, usá punto.")
     else:
-        saldo = deuda - pago 
-        if saldo == 0:
-            print("Deuda Cancelada")
-        elif saldo > 0 :
-            print("Todavia tiene deuda")
+        
+        if deuda < 0 or pago <0:
+            print("Error: La deuda y el pago no pueden ser negativos.")
         else:
-            print("El pago supera la deuda")
-        print(f"\nCliente: {cliente}")
-        print(f"Saldo pendiente: ${saldo:.2f}")
+            saldo = deuda - pago 
+            if saldo == 0:
+                print("Deuda Cancelada")
+            elif saldo > 0 :
+                print("Todavia tiene deuda")
+            else:
+                print("El pago supera la deuda")
+            print(f"\nCliente: {cliente}")
+            print(f"Saldo pendiente: ${saldo:.2f}")
+    continuar = input("\n¿Consutar otro cliente? (s/n): ").strip().lower()
+    if continuar != "s":
+        print("Gracias por usar el gestor.")
+        break
