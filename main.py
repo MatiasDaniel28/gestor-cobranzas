ac662c9afb2873e76fa16dd1d72175b8ddf4d844
Matias Daniel Ibañez <sm.matias370@gmail.com>
2026-09-11 23:13:51 -0300

    Maneja entradas no numericas

--- main.py
+++ main.py
@@ -3,18 +3,23 @@
 print ("===Gestor de cobranzas ===")
 
 cliente = input("Nombre del cliente: ")
-deuda = float(input("Deuda inicial: "))
-pago = float(input("Pago recibido: "))
 
-if deuda < 0 or pago <0:
-    print("Error: La deuda y el pago no pueden ser negativos.")
+try:
+    deuda = float(input("Deuda inicial: "))
+    pago = float(input("Pago recibido: "))
+except ValueError:
+    print("Error: ingresá números. Para decimales, usá punto.")
 else:
-    saldo = deuda - pago 
-    if saldo == 0:
-        print("Deuda Cancelada")
-    elif saldo > 0 :
-        print("Todavia tiene deuda")
+     
+    if deuda < 0 or pago <0:
+        print("Error: La deuda y el pago no pueden ser negativos.")
     else:
-        print("El pago supera la deuda")
-    print(f"\nCliente: {cliente}")
-    print(f"Saldo pendiente: ${saldo:.2f}")
+        saldo = deuda - pago 
+        if saldo == 0:
+            print("Deuda Cancelada")
+        elif saldo > 0 :
+            print("Todavia tiene deuda")
+        else:
+            print("El pago supera la deuda")
+        print(f"\nCliente: {cliente}")
+        print(f"Saldo pendiente: ${saldo:.2f}")
